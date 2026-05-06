fe9dbe7d39d73989cdfc4032c63a29e285d531cc
Qiwen Wang <wqw547243068@163.com>
2026-05-06 16:28:17 +0800

    修复已知bug：官方文件缺失一些工具包

--- python-backend/main.py
+++ python-backend/main.py
@@ -40,6 +40,19 @@ app.add_middleware(
 )
 
 chat_server = AirlineServer()
+# 【2026-4-29】 修复已知bug：官方文件缺失一些工具包
+from load_env import base_url, api_key, model_name
+from openai import AsyncOpenAI
+# NameError: name 'set_default_openai_client' is not defined
+from agents import set_default_openai_client, set_default_openai_api, set_tracing_disabled
+
+custom_client = AsyncOpenAI(
+	base_url=base_url,
+	api_key=api_key,
+)
+set_default_openai_client(client=custom_client, use_for_tracing=False)
+set_default_openai_api("chat_completions")
+set_tracing_disabled(disabled=True)
 
 
 def get_server() -> AirlineServer:
